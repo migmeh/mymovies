b8e53fbd683a3bd2b39215716f96a0af81a860d9
migmeh <xmigmeh@gmail.com>
2023-10-16 17:37:39 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ export default function App() {
         // para implementar con el proxy del otro proyecto descarguelo, y corra el proxy despues use httP://localhost:8080 sin apikey
         const apiKey = 'f5a8915645501a2493727d6b09cbabd0';
         const laurl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}&media_type=movie`;
-        const laurlDos = 'http://localhost:8080'; //este es la url del proxi necesitas correrlo para cambiar
+        //const laurlDos = 'http://localhost:8080'; //este es la url del proxi necesitas descomentar
         let resp = await axios.get(laurl); //cambiar la bariable si el proxy esta corriendo
         setResults(resp.data.results);
         console.log(resp.data.results);
